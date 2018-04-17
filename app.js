caaae045250dfb7d46ff452a8c6446aa5a226d33
GoldbeardSea <timbusch1@gmail.com>
2018-04-16 18:46:24 -0700

    shifting to work on counter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log('JS is linked');
-
+var counter = 0;
 var buttonOne = document.getElementById('button-one');
 var buttonTwo = document.getElementById('button-two');
 var buttonThree = document.getElementById('button-three');
@@ -10,60 +10,82 @@ var prodImgTwo = document.getElementById('two');
 var prodImgThree = document.getElementById('three');
 
 
-function Product(url) {
+function Product(name, url) {
+  this.name = 'name';
   this.url = url;
   this.votes = 0;
+  this.iterations = 0;
+  this.counter = 0;
 }
 
 var allProducts = [
-  new Product('img/bag.jpg'),
-  new Product('img/banana.jpg'),
-  new Product('img/bathroom.jpg'),
-  new Product('img/boots.jpg'),
-  new Product('img/breakfast.jpg'),
-  new Product('img/bubblegum.jpg'),
-  new Product('img/chair.jpg'),
-  new Product('img/cthulhu.jpg'),
-  new Product('img/dog-duck.jpg'),
-  new Product('img/dragon.jpg'),
-  new Product('img/pen.jpg'),
-  new Product('img/pet-sweep.jpg'),
-  new Product('img/scissors.jpg'),
-  new Product('img/shark.jpg'),
-  new Product('img/sweep.png'),
-  new Product('img/tauntaun.jpg'),
-  new Product('img/unicorn.jpg'),
-  new Product('img/usb.gif'),
-  new Product('img/water-can.jpg'),
-  new Product('img/wine-glass.jpg'),
+  new Product('bag', 'img/bag.jpg'),
+  new Product('banana', 'img/banana.jpg'),
+  new Product('bathroom', 'img/bathroom.jpg'),
+  new Product('boots', 'img/boots.jpg'),
+  new Product('breakfast', 'img/breakfast.jpg'),
+  new Product('bubblegum', 'img/bubblegum.jpg'),
+  new Product('chair', 'img/chair.jpg'),
+  new Product('cthulu', 'img/cthulhu.jpg'),
+  new Product('dog duck', 'img/dog-duck.jpg'),
+  new Product('dragon meat', 'img/dragon.jpg'),
+  new Product('pen', 'img/pen.jpg'),
+  new Product('pet-sweetp', 'img/pet-sweep.jpg'),
+  new Product('scissors', 'img/scissors.jpg'),
+  new Product('shark', 'img/shark.jpg'),
+  new Product('sweep', 'img/sweep.png'),
+  new Product('Tauntaun', 'img/tauntaun.jpg'),
+  new Product('unicorn', 'img/unicorn.jpg'),
+  new Product('tentacle usb', 'img/usb.gif'),
+  new Product('water-can', 'img/water-can.jpg'),
+  new Product('wine-glass', 'img/wine-glass.jpg'),
 ];
 
 var productOne = allProducts[0];
 var productTwo = allProducts[1];
 var productThree = allProducts[2];
-
+var counter = 0;
+// var previousProduct = {
+//   one: null,
+//   two: null,
+//   three: null,
+// }
 
 buttonOne.addEventListener('click', function(e) {
+  counter++;
   productOne.votes++;
+  pickNewProduct();
+});
+
+buttonTwo.addEventListener('click', function(e) {
+  counter++;
+  productTwo.votes++;
+  pickNewProduct();
+});
+
+buttonThree.addEventListener('click', function(e) {
+  counter++;
+  productThree.votes++;
+  pickNewProduct();
+});
+
+function pickNewProduct() {
   productOne = allProducts[Math.floor(Math.random() * allProducts.length)];
   prodImgOne.src = productOne.url;
   productTwo = allProducts[Math.floor(Math.random() * allProducts.length)];
   prodImgTwo.src = productTwo.url;
   productThree = allProducts[Math.floor(Math.random() * allProducts.length)];
   prodImgThree.src = productThree.url;
-});
+  while (productOne.url === productTwo.url ||
+    productOne.url === productThree.url ||
+    productTwo.url === productThree.url) {
+    productOne = allProducts[Math.floor(Math.random() * allProducts.length)];
+    prodImgOne.src = productOne.url;
+    productTwo = allProducts[Math.floor(Math.random() * allProducts.length)];
+    prodImgTwo.src = productTwo.url;
+    productThree = allProducts[Math.floor(Math.random() * allProducts.length)];
+    prodImgThree.src = productThree.url;
+  }
+}
 
-buttonTwo.addEventListener('click', function(e) {
-  productTwo.votes++;
-  productTwo = allProducts[Math.floor(Math.random() * allProducts.length)];
-  prodImgTwo.src = productTwo.url;
-
-});
-
-buttonThree.addEventListener('click', function(e) {
-  productThree.votes++;
-  productThree = allProducts[Math.floor(Math.random() * allProducts.length)];
-  prodImgThree.src = productThree.url;
-
-});
-
+pickNewProduct();
