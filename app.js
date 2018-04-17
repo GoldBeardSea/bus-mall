6e51d2c3a82ff9da807a0a93ac1724856ff5fec3
GoldbeardSea <timbusch1@gmail.com>
2018-04-16 17:16:31 -0700

    working on function

--- app.js
+++ app.js
@@ -5,24 +5,65 @@ console.log('JS is linked');
 var buttonOne = document.getElementById('button-one');
 var buttonTwo = document.getElementById('button-two');
 var buttonThree = document.getElementById('button-three');
+var prodImgOne = document.getElementById('one');
+var prodImgTwo = document.getElementById('two');
+var prodImgThree = document.getElementById('three');
 
-var buttonOneVotes = 0;
-var buttonTwoVotes = 0;
-var buttonThreeVotes = 0;
+
+function Product(url) {
+  this.url = url;
+  this.votes = 0;
+}
+
+var allProducts = [
+  new Product('img/bag.jpg'),
+  new Product('img/banana.jpg'),
+  new Product('img/bathroom.jpg'),
+  new Product('img/boots.jpg'),
+  new Product('img/breakfast.jpg'),
+  new Product('img/bubblegum.jpg'),
+  new Product('img/chair.jpg'),
+  new Product('img/cthulhu.jpg'),
+  new Product('img/dog-duck.jpg'),
+  new Product('img/dragon.jpg'),
+  new Product('img/pen.jpg'),
+  new Product('img/pet-sweep.jpg'),
+  new Product('img/scissors.jpg'),
+  new Product('img/shark.jpg'),
+  new Product('img/sweep.png'),
+  new Product('img/tauntaun.jpg'),
+  new Product('img/unicorn.jpg'),
+  new Product('img/usb.gif'),
+  new Product('img/water-can.jpg'),
+  new Product('img/wine-glass.jpg'),
+];
+
+var productOne = allProducts[0];
+var productTwo = allProducts[1];
+var productThree = allProducts[2];
 
 
 buttonOne.addEventListener('click', function(e) {
-  buttonOneVotes++;
+  productOne.votes++;
+  productOne = allProducts[Math.floor(Math.random() * allProducts.length)];
+  prodImgOne.src = productOne.url;
+  productTwo = allProducts[Math.floor(Math.random() * allProducts.length)];
+  prodImgTwo.src = productTwo.url;
+  productThree = allProducts[Math.floor(Math.random() * allProducts.length)];
+  prodImgThree.src = productThree.url;
 });
 
 buttonTwo.addEventListener('click', function(e) {
-  buttonTwoVotes++;
+  productTwo.votes++;
+  productTwo = allProducts[Math.floor(Math.random() * allProducts.length)];
+  prodImgTwo.src = productTwo.url;
+
 });
 
 buttonThree.addEventListener('click', function(e) {
-  buttonThreeVotes++;
+  productThree.votes++;
+  productThree = allProducts[Math.floor(Math.random() * allProducts.length)];
+  prodImgThree.src = productThree.url;
+
 });
 
-console.log(buttonOneVotes);
-console.log(buttonTwoVotes);
-console.log(buttonThreeVotes);
